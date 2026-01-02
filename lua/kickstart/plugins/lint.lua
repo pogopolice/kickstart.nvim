e3bd38bded06c58390cad8d84d535ac799a440a2
James Pasteur <pogopolice@gmail.com>
2026-01-01 22:43:16 -0500

    Plugins
    enable debug
    
    enable lint
    
    Linters
    
    enable bash linter
    
    enable cloudformation linter

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,10 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        sh = { 'shellcheck' },
+        bash = { 'shellcheck' },
+        yaml = { 'cfn_lint' },
+        json = { 'cfn_lint' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
